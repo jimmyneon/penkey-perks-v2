1f40c5a4bb989df3d6c58df26d02f4e59141c905
John Hopwood <johnhopwood@Johns-MacBook-Air.local>
2026-06-04 17:40:44 +0100

    Fix stamper position to be more central, delete full-size voucher PNGs, use icon versions as primary

--- supabase/migrations/20260603000001_add_voucher_image_urls.sql
+++ supabase/migrations/20260603000001_add_voucher_image_urls.sql
@@ -1,23 +1,23 @@
 -- Add image_url column to voucher_templates
 ALTER TABLE public.voucher_templates ADD COLUMN IF NOT EXISTS image_url TEXT;
 
--- Update existing voucher templates with image URLs
+-- Update existing voucher templates with image URLs (using 500px icon versions)
 UPDATE public.voucher_templates 
-SET image_url = '/vouchers/syrup.png'
+SET image_url = '/vouchers/syrup-icon.png'
 WHERE name = 'Free Syrup';
 
 UPDATE public.voucher_templates 
-SET image_url = '/vouchers/buscuit.png'
+SET image_url = '/vouchers/buscuit-icon.png'
 WHERE name = 'Free Biscuit';
 
 UPDATE public.voucher_templates 
-SET image_url = '/vouchers/coffeetea.png'
+SET image_url = '/vouchers/coffeetea-icon.png'
 WHERE name = 'Free Coffee or Tea';
 
 UPDATE public.voucher_templates 
-SET image_url = '/vouchers/milkshake.png'
+SET image_url = '/vouchers/milkshake-icon.png'
 WHERE name = 'Milkshake Voucher';
 
 UPDATE public.voucher_templates 
-SET image_url = '/vouchers/hotbap.png'
+SET image_url = '/vouchers/hotbap-icon.png'
 WHERE name = 'Premium Hot Bun';
